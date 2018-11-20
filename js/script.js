61ee4901aa7a4918afed094f3107a0d0c10ca45a
Olegun81 <ukrainianboy81@gmail/com>
2018-11-20 16:15:38 +0200

    added responsive header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 
+$( document ).ready(function() {
+
+    $(function() {
+    $(".header_mobile").click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass("active");
+        if ($('.header_nav').hasClass('header_nav_active')) {
+            $('.header_nav').removeClass('header_nav_active');
+        }
+        else {
+            $('.header_nav').addClass('header_nav_active');
+        }
+        
+    });
+});
+
+
+
+
+});
